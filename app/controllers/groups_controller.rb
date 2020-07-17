9430f1cec37c693294f98d536720b50382c40508
ea2ne <ea2ne@yahoo.co.jp>
2020-07-17 11:12:47 +0900

    グループ編集実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,7 +6,31 @@ class GroupsController < ApplicationController
   end
 
   def create
-    binding.pry
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループ作ったよ'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to root_path, notice: 'グループw
+      更新したよ'
+    else
+      render :edit
+    end
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, user_ids: [] )
   end
 
 end

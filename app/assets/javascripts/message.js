f5af704d40647ece7dcdef767a5d4b6960212593
ea2ne <ea2ne@yahoo.co.jp>
2020-07-24 11:14:03 +0900

    イベント発火でmessages#create作動

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,16 @@
 $(function(){
   $('.Form').on('submit', function(e){
-    e.preventDefault()
+    e.preventDefault();
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,  //同期通信でいう『パス』
+      type: 'POST',  //同期通信でいう『HTTPメソッド』
+      data: formData,  
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
     console.log("お腹減った")
   });
 });

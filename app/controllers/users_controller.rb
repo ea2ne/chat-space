97f8f1660b4a027d73f3a1b53e21de46c0fdfefe
ea2ne <ea2ne@yahoo.co.jp>
2020-07-25 19:55:09 +0900

    ルーティングなどAPI準備

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
 
+  def index
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+  
   def edit
   end
 
@@ -20,4 +27,5 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
 end
